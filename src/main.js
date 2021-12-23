7a68e3f9a86b4e49f6c6441e5f3154e196fe7e60
Mike Blemberg <mikeblemberg@gmail.com>
2021-12-22 22:58:27 -0600

    initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const chessground_1 = require("chessground");
+const config = {};
+const ground = (0, chessground_1.Chessground)(document.body, config);
